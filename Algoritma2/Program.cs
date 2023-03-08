09e0989454f280632aab23243c40613f39d50870
rmutluer <resul.mutluer@hotmail.com>
2023-03-08 21:08:38 +0300

    Algoritma2

---
+++ Algoritma2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algoritma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin giriniz:");
+            string metin = Console.ReadLine();
+            char[] metinDizi = metin.ToCharArray();
+            char ilkHarf = metin[0];
+            for (int i = 1; i < metin.Length; i++)
+            {
+                metinDizi[i-1] = metin[i];
+            }
+            metinDizi[metinDizi.Length - 1] = ilkHarf;
+            Console.WriteLine(metinDizi);
+        }
+    }
+}
