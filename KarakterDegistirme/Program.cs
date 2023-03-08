68ff182d713f6b5cbc909db40f11ade243bbe2d5
rmutluer <resul.mutluer@hotmail.com>
2023-03-09 00:53:21 +0300

    KarakterDegistirme

---
+++ KarakterDegistirme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KarakterDegistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin giriniz:");
+            string metin = Console.ReadLine();
+            char[] metinDizi = metin.ToCharArray();
+            char ilkHarf = metin[0];
+            metinDizi[0] = metinDizi[metinDizi.Length - 1];
+            metinDizi[metinDizi.Length - 1] = ilkHarf;
+            Console.WriteLine(metinDizi);
+        }
+    }
+}
