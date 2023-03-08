3e81bda68fb7a00e7f4c8d2f7a87ef2676b44107
rmutluer <resul.mutluer@hotmail.com>
2023-03-08 20:08:51 +0300

    Daire

---
+++ DaireCiz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DaireCiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.Write("Çizmek istediğiniz dairenin yarıçapını giriniz: ");
+            int yaricap = int.Parse(Console.ReadLine());
+
+            for (int a = -yaricap; a <= yaricap; a++)
+            {
+                if (a<0)
+                {
+
+                }
+                for (int b = -yaricap; b <= yaricap; b++)
+                {
+                    if (b * b + a * a <= yaricap * yaricap)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("x");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
