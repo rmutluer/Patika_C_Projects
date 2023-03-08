d0ec9f1e1fc6cd4e2089c2721b436e1f008a4a36
rmutluer <resul.mutluer@hotmail.com>
2023-03-08 20:52:24 +0300

    Algoritma

---
+++ Algoritma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen içinden gireceğiniz indexteki karakteri çıkartacağınız bir metin girip virgül ekledikten sonra çıkarmak istediğiniz indexi giriniz.");
+            string metin = Console.ReadLine();
+            if (metin.Contains(","))
+            {
+                string [] metinDizi= metin.Split(",");
+                int index = int.Parse(metinDizi[1]);
+                if (index<metinDizi[0].Length)
+                {
+                    for (int i = 0; i < metinDizi[0].Length; i++)
+                    {
+                        if (i!=index)
+                        {
+                            Console.Write(metinDizi[0][i]);
+                        }
+                    }
+                    
+                }
+                
+
+            }
+        }
+    }
+}
