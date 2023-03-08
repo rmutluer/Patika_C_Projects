58a30bedd4b6025008b5fdcf33536aedab02eda9
rmutluer <resul.mutluer@hotmail.com>
2023-03-09 01:12:07 +0300

    SessizHarf

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SessizHarf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir metin giriniz:");
+            string metin = Console.ReadLine();
+            metin = metin.ToLower();
+            bool sonuc = false;
+            for (int i = 1; i < metin.Length; i++)
+            {
+                if (metin[i] != 'a' && metin[i] != 'e' && metin[i] != 'ı' && metin[i] != 'i' && metin[i] != 'o' && metin[i] != 'ö' && metin[i] != 'u' && metin[i] != 'ü' && metin[i - 1] != 'a' && metin[i - 1] != 'e' && metin[i - 1] != 'ı' && metin[i - 1] != 'i' && metin[i - 1] != 'o' && metin[i - 1] != 'ö' && metin[i - 1] != 'u' && metin[i - 1] != 'ü')
+                {
+                    sonuc = true;
+                }
+            }
+            Console.WriteLine(sonuc);
+        }
+    }
+}
